e7b545d557047de466eac0bd957847cdcdc2b6fb
Dikshant67 <waghdikshant5@gmail.com>
2025-04-17 21:28:19 +0530

    WorldWise added

---
+++ worldwise/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="" element={<Homepage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
