e32ba7825eae0422c11120296656dd8bfe4015e4
Dikshant67 <waghdikshant5@gmail.com>
2025-04-19 12:32:42 +0530

    added Routes and pages

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,16 +2,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="product" element={<Product />}></Route>
-        <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="" element={<Homepage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route index element={<Homepage />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List from index</p>}></Route>
+            <Route path="cities" element={<p>List of Cities</p>}></Route>
+            <Route path="countries" element={<p>countries</p>}></Route>
+            <Route path="form" element={<p>Form</p>}></Route>
+          </Route>
+          <Route path="login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
