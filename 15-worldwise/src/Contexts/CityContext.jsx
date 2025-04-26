853e7a7a08404ae7b12a500f14ae81039ccf1518
Dikshant67 <waghdikshant5@gmail.com>
2025-04-26 14:33:49 +0530

    Added Map using React Leaflet library

---
+++ 15-worldwise/src/Contexts/CityContext.jsx
@@ -0,0 +1,49 @@
+import {createContext, useContext, useEffect, useState} from "react";
+const BASE_URL = "https://my-json-server.typicode.com/Dikshant67/questions";
+const CitiesContext =createContext();
+// eslint-disable-next-line react/prop-types
+function CitiesProvider({children}) {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [currentCity,setCurrentCity] = useState({});
+    useEffect(function () {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                setCities(data);
+            } catch {
+                alert("there was a error loading data");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchCities();
+    }, []);
+
+    async function getCity(id){
+
+        try {
+            setIsLoading(true);
+            const res = await fetch(`${BASE_URL}/cities/${id}`);
+            const data = await res.json();
+            setCurrentCity(data);
+        } catch {
+            alert("there was a error loading data");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+    return <CitiesContext.Provider value={{
+        cities,isLoading,currentCity,getCity
+    }} >{children}</CitiesContext.Provider>
+}
+function useCities() {
+    const context = useContext(CitiesContext);
+    if(context===undefined) { throw new Error("CityContext used outside context"); }
+    return context;
+
+}
+
+export {CitiesProvider,useCities};
