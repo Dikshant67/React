ba480a083831f83959007eb4e065b3141ebbacd0
Dikshant67 <waghdikshant5@gmail.com>
2025-04-26 11:53:30 +0530

    Added URL navigation using useNavigate,useSearchParamaters hookes

--- worldwise/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './Button.module.css';
-function Button({ onClick ,children, type}) {
-    return (
-        <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>{children}</button>
-    );
-}
-
-export default Button;
--- worldwise/src/components/ButtonBack.jsx
+++
@@ -1,9 +0,0 @@
-
-
-function ButtonBack(props) {
-    return (
-        <div></div>
-    );
-}
-
-export default ButtonBack;
